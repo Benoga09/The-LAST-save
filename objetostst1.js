6b97579496a19504c8728a0d12e3a24981972796
Benoga09 <beni_nogueira_carvalho@outlook.com>
2024-10-04 20:08:49 -0300

    Delete objetostst1.js

--- objetostst1.js
+++
@@ -1,9 +0,0 @@
-const professor = {
-nome: 'Vitor',
-idade: 27,
-tarfas: ["dar aula","responder"],
-contarPiada: function(){
-    this.console.log("é pa vê ou pa cumê?")
-
-}}
-console.log(professor)
